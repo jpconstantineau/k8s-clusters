8d092f642c96a6ca6448fb78827d9a4c5ed23597
Pierre Constantineau <jpconstantineau@gmail.com>
2022-03-17 22:37:35 -0600

    updated to k3sup

---
+++ provision/k3sup.sh
@@ -0,0 +1,7 @@
+
+
+
+k3sup install --ip 129.153.66.137 --user opc  --ssh-key $HOME/.ssh/local-pi-id_ed25519
+
+k3sup join --ip 129.146.169.239 --user opc --server-ip 129.153.66.137 --server-user opc  --ssh-key $HOME/.ssh/local-pi-id_ed25519
+k3sup join --ip 141.148.167.233 --user opc --server-ip 129.153.66.137 --server-user opc  --ssh-key $HOME/.ssh/local-pi-id_ed25519
